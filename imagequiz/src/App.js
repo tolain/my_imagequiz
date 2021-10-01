aab9f8181afb77ee37d153fb16a2050240ca2d66
tolain <90009568+tolain@users.noreply.github.com>
2021-10-01 00:01:18 -0700

    Update App.js

--- imagequiz/src/App.js
+++ imagequiz/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import SignUp from './components/SignUp'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br /><br />
+      <img src = "https://habahram.blob.core.windows.net/flowers/acacia.jpg" alt="">
+      <br /><br />
+      <div>Acacia</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/alyssum.jpg" alt="">
+      <br /><br />
+      <div>Alyssum</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/amaryllis.jpg" alt="">
+      <br /><br />
+      <div>Amaryllis</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/aster.jpg" alt="">
+      <br /><br />
+      <div>Aster</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/azalea.jpg" alt="">
+      <br /><br />
+      <div>Azalea</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/begonia.jpg" alt="">
+      <br /><br />
+      <div>Begonia</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/buttercup.jpg" alt="">
+      <br /><br />
+      <div>Buttercup</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/calluna.jpg" alt="">
+      <br /><br />
+      <div>Calluna</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/camellia.jpg" alt="">
+      <br /><br />
+      <div>Camellia</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/cardinal.jpg" alt="">
+      <br /><br />
+      <div>Cardinal</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/carnation.jpg" alt="">
+      <br /><br />
+      <div>Carnation</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/clover.jpg" alt="">
+      <br /><br />
+      <div>Clover</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/crownimperial.jpg" alt="">
+      <br /><br />
+      <div>Crown Imperial</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/daffodil.jpg" alt="">
+      <br /><br />
+      <div>Daffodil</div>
+      <img src = "https://habahram.blob.core.windows.net/flowers/dahlia.jpg" alt="">
+      <br /><br />
+      <div>Dahlia</div>
+      <br />
     </div>
   );
 }
